3a874df44d8fcae309c9cacc69c0dded75be011c
Felipe <feslima93@gmail.com>
2022-02-15 10:13:10 -0300

    Logout and company list for user docstrings

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from typing import cast
 
 from django.contrib.auth import logout
 from django.db.models import QuerySet
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -30,9 +31,12 @@ class LoginView(CreateAPIView):
 
 
 class LogoutView(APIView):
+    """Logout authenticated users on POST."""
+
     permission_classes = (IsAuthenticated,)
     http_method_names = ["post"]
 
+    @extend_schema(responses={204: None})
     def post(self, request: Request) -> Response:
         logout(request)
 
@@ -40,6 +44,8 @@ class LogoutView(APIView):
 
 
 class CompanyListView(ListAPIView):
+    """List all the companies for current authenticated user."""
+
     serializer_class = CompanySerializer
     permission_classes = (IsAuthenticated,)
 
